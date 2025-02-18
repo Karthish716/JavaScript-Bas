e1feee0412eb64031964a7d1beb177a1d2151899
Karthish716 <karthish2001@gmail.com>
2025-02-18 15:24:05 +0530

    feat: Js basics

---
+++ numbers.js
@@ -0,0 +1,28 @@
+let pi = 3.14;
+let prime=29;
+
+let temp = NaN;
+temp = NaN ** 0;
+
+console.log(temp)
+
+
+let limit = 9007199254740991n;
+console.log(limit+1n);
+console.log(limit+2n);
+console.log(limit+3n);
+
+
+// String Data Type
+
+let intro = "Hii This is code";
+
+console.log(intro)
+
+let para =`This a man's home, "iam bat man "`;
+let nums =`1+2 = ${1+2}`;
+console.log(nums);
+
+let isAldult = true;
+isAldult = String(isAldult);
+console.log(typeof isAldult)
---
+++ script.js
@@ -0,0 +1,5 @@
+let bookss;
+bookss="hiii",f="heloo";
+console.log(bookss + " "+f);
+
+const DATE_OF_BIRTH = '1.1.1';
